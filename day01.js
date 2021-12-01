6a2812e53b2ce55b61e4a789b61b6bce9e251c01
John Donnellan <codybartfast@users.noreply.github.com>
2021-12-01 10:28:42 +0000

    🔑

--- day01.js
+++ day01.js
@@ -2,14 +2,30 @@ const fs = require('fs');
 
 fs.readFile('day01.txt', 'utf-8', (err, data) => {
     if (err) throw err;
-    sonarSweep1(data.trim().split('\n'));
+    let lines = data.trim().split('\n');
+    sonarSweep1(lines);
+    sonarSweep2(lines);
 });
 
 function sonarSweep1(lines){
     let depths = lines.map(Number)
+    console.log(countIncreases(depths));
+}
+
+function countIncreases(depths){
     let increaseCount = 0;
     for(i = 1; i < depths.length; i++){
         if(depths[i] > depths[i - 1]) increaseCount++;
     }
-    console.log(increaseCount);
+    return increaseCount;
+}
+
+function sonarSweep2(lines){
+    let depths = lines.map(Number)
+    let sliding = [];
+    for(i = 2; i < depths.length; i++){
+        sliding =
+            sliding.concat(depths[i - 2] + depths[i - 1] + depths[i]);
+    }
+    console.log(countIncreases(sliding));
 }
