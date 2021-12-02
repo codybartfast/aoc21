cb692fd68d7890295f90bf42dfbd16d44ad660c7
John Donnellan <codybartfast@users.noreply.github.com>
2021-12-02 09:52:14 +0000

    ⚟

--- day02.js
+++ day02.js
@@ -5,6 +5,7 @@ fs.readFile('day02.txt', 'utf-8', (err, input) => {
     let lines = input.trim().split('\n');
     let data = lines.map(parseLine);
     dive1(data);
+    dive2(data);
 });
 
 function parseLine(line){
@@ -26,6 +27,27 @@ function dive1(data){
                 break;
             case 'down':
                 depth += dist;
+                break;
+        }
+    });
+    console.log(depth * range);
+}
+
+function dive2(data){
+    let aim =0, depth = 0, range = 0;
+    data.forEach(instr => {
+        let dist = instr.distance;
+        switch(instr.direction){
+            case 'forward':
+                range += dist;
+                depth += (dist * aim)
+                depth = Math.max(0, depth);
+                break;
+            case 'up':
+                aim -= dist;
+                break;
+            case 'down':
+                aim += dist;
                 break;
         }
     });
