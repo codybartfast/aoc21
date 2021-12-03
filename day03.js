efc6cddb32d19ac96fa91240a1eb9cac206bc5c1
John Donnellan <codybartfast@users.noreply.github.com>
2021-12-03 09:30:58 +0000

    🗲

--- day03.js
+++ day03.js
@@ -6,7 +6,8 @@ fs.readFile('day03.txt', 'utf-8', (err, input) => {
     if (err) throw err;
     let lines = input.trim().split(/\r?\n/);
     let data = lines;
-    binaryDiagnostic1(data);
+    cl(binaryDiagnostic1(data));
+    cl(binaryDiagnostic2(data));
 });
 
 function binaryDiagnostic1(data) {
@@ -19,18 +20,71 @@ function binaryDiagnostic1(data) {
             }
         }
     });
-    let gamma = rate(data.length, posCounts, false);
-    let epsilon = rate(data.length, posCounts, true);
-    cl(gamma * epsilon);
+    let gamma = gammaRate(data.length, posCounts);
+    let epsilon = epsilonRate(data.length, posCounts);
+    return gamma * epsilon;
 }
 
-function rate(numCount, posCounts, epsilon) {
+function gammaRate(numCount, posCounts) {
+    return rate1(numCount, posCounts, false);
+}
+
+function epsilonRate(numCount, posCounts) {
+    return rate1(numCount, posCounts, true);
+}
+
+function rate1(numCount, posCounts, epsilon) {
     if (epsilon) {
         posCounts = posCounts.map(c => numCount - c);
     }
-    const threshold = numCount / 2 + 1;
-    const binDigits = posCounts.map(c => c >= threshold ? "1" : "0");
+    const threshold = numCount / 2;
+    const binDigits = posCounts.map(c => c > threshold ? "1" : "0");
     const binString = binDigits.join('');
     const rate = parseInt(binString, 2);
     return rate;
+}
+
+function binaryDiagnostic2(data) {
+    const o2 = oxygenGeneratorRating(data);
+    const co2 = co2ScrubberRating(data);
+    return o2 * co2;
+}
+
+function oxygenGeneratorRating(data) {
+    return rating2(data, 0, false);
+}
+
+function co2ScrubberRating(data) {
+    return rating2(data, 0, true);
+}
+
+function rating2(data, i, pickFewer) {
+    let size = data.length;
+    if (size === 0) {
+        throw new Error('No data!');
+    }
+    else if (size === 1) {
+        return parseInt(data[0], 2);
+    }
+
+    const zeros = [];
+    const ones = [];
+    data.forEach(num => {
+        const b = num[i];
+        if (b === "0") {
+            zeros.push(num);
+        } else if (b === "1") {
+            ones.push(num);
+        } else {
+            throw new Error(
+                `Unexpected character at position ${i} in ${num}: ${b}`);
+        }
+    });
+
+    let nextNums =
+        ones.length >= zeros.length ?
+            (pickFewer ? zeros : ones) :
+            (pickFewer ? ones : zeros);
+
+    return rating2(nextNums, i + 1, pickFewer);
 }
