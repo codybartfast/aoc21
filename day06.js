e938e83b06b1e5b8894a9f43b9ca8a36438f62cc
John Donnellan <codybartfast@users.noreply.github.com>
2021-12-19 11:46:40 +0000

    🐟

--- day06.js
+++ day06.js
@@ -5,13 +5,14 @@ const cl = console.log;
 fs.readFile('day06.txt', 'utf-8', (err, input) => {
     if (err) throw err;
     let fish = input.trim().split(',').map(Number);
-    cl(lanternfish(fish));
+    cl(lanternfish(fish, 80));
+    cl(lanternfish(fish, 256));
 });
 
-function lanternfish(fish) {
+function lanternfish(fish, days) {
     let shoal = new Array(9).fill(0);
     fish.forEach(f => shoal[f]++)
-    for (let i = 0; i < 80; i++) {
+    for (let i = 0; i < days; i++) {
         day(shoal);
     }
     return shoal.reduce((a, b) => a + b);
