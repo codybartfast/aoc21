9ddee72741532b4dc037c8573c9aa3172783bfa4
John Donnellan <codybartfast@users.noreply.github.com>
2021-12-22 14:07:29 +0000

    ⛖

--- day12.js
+++ day12.js
@@ -5,14 +5,15 @@ const cl = console.log;
 fs.readFile('day12.txt', 'utf-8', (err, input) => {
     if (err) throw err;
     let pairs = input.trim().split(/\r?\n/).map(ln => ln.split('-'));
-    cl(passageParsing(pairs));
+    cl(passageParsing(pairs, false));
+    cl(passageParsing(pairs, true));
 });
 
-function passageParsing(pairs) {
+function passageParsing(pairs, mayRevist) {
     let connections = findConnections(pairs);
     let pathCount = 0;
-    for (let path of findPaths(connections, ['start'])) {
-        pathCount ++;
+    for (let _ of findPaths(connections, ['start'], !mayRevist)) {
+        pathCount++;
     }
     return pathCount;
 }
@@ -28,14 +29,23 @@ function findConnections(pairs) {
     return connections;
 }
 
-function* findPaths(connections, path) {
+function* findPaths(connections, path, alreadyRevisited) {
     let here = path.at(-1);
     if (here === 'end') yield path;
-    // cl('here', here);
     for (let next of connections.get(here)) {
+        let goNext = false;
+        let haveRevisited = alreadyRevisited;
         if (/[A-Z]/.test(next) || !path.includes(next)) {
+            goNext = true;
+        }
+        else if (!alreadyRevisited && next.length <= 2) {
+            haveRevisited = true;
+            goNext = true;
+        }
+
+        if (goNext) {
             path.push(next);
-            yield* findPaths(connections, path);
+            yield* findPaths(connections, path, haveRevisited);
         }
     }
     path.pop();
