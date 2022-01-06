4973d5a5754ae086a095f4f5a88e1b45a18edb9e
John Donnellan <john@codybartfast.com>
2022-01-06 14:08:06 +0000

    📻

--- day16.js
+++ day16.js
@@ -1,26 +1,47 @@
 'use strict';
-const exp = require('constants');
 const fs = require('fs');
 const cl = console.log;
 
 fs.readFile('day16.txt', 'utf-8', (err, input) => {
     if (err) throw err;
-    let transmisionions =
-        input.trim().split(/\r?\n/)
-            .map(parse)
-            .map(bits => new Transmission(bits));
-    for (let trans of transmisionions) {
-        const pkt = readPacket(trans);
-        cl(eval1(pkt));
-    }
+    const transmission = new Transmission(hexToBits(input.trim()));
+    const packet = readPacket(transmission);
+    cl(packetDecoder1(packet));
+    cl(packetDecoder2(packet));
 });
 
-function eval1(packet) {
-    let vSum = packet.version
+function packetDecoder1(packet) {
+    let verSum = packet.version
     if (packet.type !== 4) {
-        vSum += packet.payload.map(eval1).reduce((a, b) => a + b);
+        verSum +=
+            packet.payload.map(packetDecoder1).reduce((a, b) => a + b);
     }
-    return vSum;
+    return verSum;
+}
+
+function packetDecoder2(packet) {
+    if (packet.type === 4) {
+        return packet.payload;
+    }
+    const args = packet.payload.map(packetDecoder2);
+    switch (packet.type) {
+        case 0:
+            return args.reduce((a, b) => a + b);
+        case 1:
+            return args.reduce((a, b) => a * b);
+        case 2:
+            return Math.min(...args);
+        case 3:
+            return Math.max(...args);
+        case 5:
+            return args[0] > args[1] ? 1 : 0;
+        case 6:
+            return args[0] < args[1] ? 1 : 0;
+        case 7:
+            return args[0] === args[1] ? 1 : 0;
+        default:
+            throw `Unexpected operator type ${packet.type}`;
+    }
 }
 
 function readPacket(transmission) {
@@ -39,66 +60,51 @@ function readType(trans) {
 }
 
 function readPayload(trans, type) {
-    return type === 4 ? readLiteral(trans) : readOperator(trans, type);
-}
-
-function readOperator(trans, type) {
-    const countPackets = Number(trans.read(1));
-    const expected = parseInt(trans.read(countPackets ? 11 : 15), 2);
-    const initialReadCount = trans.readCount;
-    let subPackets = [];
-    const bitCount = () => trans.readCount - initialReadCount;
-    const pktCount = () => subPackets.length;
-    function done() {
-        const actual = countPackets ? pktCount() : bitCount();
-        return actual < expected ? false :
-            actual === expected ? true :
-                (() => { throw `Actual is greater then Expected!` })();
-    }
-
-    while (!done()) {
-        subPackets.push(readPacket(trans));
-    }
-    return subPackets;
+    return type === 4 ? readLiteral(trans) : readOperator(trans);
 }
 
 function readLiteral(trans) {
     const chunkLen = 5;
     function readChunks(num) {
         const chunk = trans.read(chunkLen);
-        num = (num << (chunkLen - 1)) + parseInt(chunk.substring(1), 2);
+        num = (num * 16) + parseInt(chunk.substring(1), 2);
         return (chunk[0] === '1') ? readChunks(num) : num;
     }
     return readChunks(0);
 }
 
+function readOperator(trans) {
+    const countPackets = Number(trans.read(1));
+    const expected = parseInt(trans.read(countPackets ? 11 : 15), 2);
+    const initialReadCount = trans.readCount;
+    const subPackets = [];
+    const bitCount = () => trans.readCount - initialReadCount;
+    const pktCount = () => subPackets.length;
+
+    function done() {
+        const actual = countPackets ? pktCount() : bitCount();
+        return actual < expected ? false :
+            actual === expected ? true :
+                (() => { throw `Actual is greater then Expected!` })();
+    }
+    while (!done()) {
+        subPackets.push(readPacket(trans));
+    }
+    return subPackets;
+}
+
 function Transmission(bitString) {
     this.bits = bitString;
     this.readCount = 0;
-    this.remaining = bitString.length;
     this.read = function (n) {
-        if (n > this.remaining) {
-            throw `Attempted to read ${n} bits but only ${this.remaining} remain`;
-        }
         const r = this.bits.substring(0, n);
         this.bits = this.bits.substring(n);
         this.readCount += n;
-        this.remaining -= n;
         return r;
     };
-
-    // this.align = function () {
-    //     const partRead = this.readCount % bitsPerHex;
-    //     if (partRead !== 0) {
-    //         this.read(bitsPerHex - partRead);
-    //     }
-    // }
 }
 
-function parse(line) {
-    return [...line].map(hexCharToBits).join('');
-}
-
-function hexCharToBits(c) {
-    return parseInt(c, 16).toString(2).padStart(4, '0');
+function hexToBits(hexStr) {
+    const hexCharToBits = c => parseInt(c, 16).toString(2).padStart(4, '0');
+    return [...hexStr].map(hexCharToBits).join('');
 }
