966c2395d48c72ced2a93a7ecb14daca8952af58
John Donnellan <john@codybartfast.com>
2022-01-27 15:45:53 +0000

    🐚

--- day23.js
+++ day23.js
@@ -1,31 +1,41 @@
 'use strict';
 const fs = require('fs');
-const { stringify } = require('querystring');
 const cl = console.log;
 
 const empty = -19;
 
 fs.readFile('day23.txt', 'utf-8', (err, input) => {
     if (err) throw err;
-    const lines = input.trim().split(/\r?\n/)
-    const rooms = [];
-    for (let i = 2; i < 9; i += 2) {
-        rooms.push({
-            pos: i,
-            top: ampiNum(lines[2][i + 1]),
-            bottom: ampiNum(lines[3][i + 1])
-        });
-    }
-    const hallway = new Array(11).fill(empty);
-    amphipod(hallway, rooms);
+    const lines1 = input.trim().split(/\r?\n/);
+    const lines2 = lines1.slice(0, 3).concat(
+        ['  #D#C#B#A#', '  #D#B#A#C#'],
+        lines1.slice(3));
+    const [hallway1, rooms1] = newBurrow(lines1);
+    cl(amphipod(hallway1, rooms1));
+    const [hallway2, rooms2] = newBurrow(lines2);
+    cl(amphipod(hallway2, rooms2));
 });
+
+function newBurrow(lines) {
+    const rooms = [];
+    for (let x = 3; x < 10; x += 2) {
+        const room = [];
+        room.pos = x - 1;
+        for (let y = 2; y < lines.length - 1; y++) {
+            room.push(ampiNum(lines[y][x]))
+        }
+        rooms.push(room);
+    }
+    return [new Array(11).fill(empty), rooms];
+}
 
 function amphipod(hallway, rooms) {
     const bestCost = { cost: Infinity };
-    for (let history of burrowCost(hallway, rooms, 0, bestCost, [])) {
-        const [_, __, cost] = history.at(-1);
-        cl(cost);
-    }
+    const histories = burrowCost(hallway, rooms, 0, bestCost, []);
+    let history;
+    for (history of histories);
+    // displayHistory(history);
+    return history.at(-1)[2];
 }
 
 function* burrowCost(hallway, rooms, cost, bestCost, history) {
@@ -37,85 +47,71 @@ function* burrowCost(hallway, rooms, cost, bestCost, history) {
     if (allRoomsDone(rooms)) {
         bestCost.cost = Math.min(bestCost.cost, cost);
         yield history;
-    } {
+    } else {
         yield* moveIn(hallway, rooms, cost, bestCost, history);
         yield* moveOut(hallway, rooms, cost, bestCost, history);
     }
 }
 
 function* moveIn(origHallway, origRooms, cost, bestCost, history) {
-    // cl('... left ampi in ...', history);
     for (let origin = 0; origin < origHallway.length; origin++) {
         if (!isEmpty(origHallway[origin])) {
             const ampi = origHallway[origin];
-            if ((roomEmpty(origRooms, ampi)
-                || roomHalfDone(origRooms, ampi))
-                && pathIsClear(origHallway, origin, origRooms[ampi].pos)
+            if (!hasVisitor(origRooms, ampi)
+                && hallIsClear(origHallway, origin, origRooms[ampi].pos)
             ) {
-
                 let [hallway, rooms] = copy(origHallway, origRooms);
                 const home = rooms[ampi];
                 let dist = Math.abs(origin - home.pos);
-                if (roomHalfDone(rooms, ampi)) {
-                    dist += 1;
-                    home.top = ampi;
-                }
-                else {
-                    dist += 2;
-                    home.bottom = ampi;
-                }
+                let i = 0;
+                for (; i < home.length && isEmpty(home[i + 1]); i++);
+                dist += (i + 1);
+                home[i] = ampi;
                 hallway[origin] = empty;
                 const newCost = cost + ampiCost(ampi) * dist;
-                yield* burrowCost(hallway, rooms, newCost, bestCost, history);
+                yield*
+                    burrowCost(hallway, rooms, newCost, bestCost, history);
             }
         }
     }
 }
 
-function pathIsClear(hallway, origin, homePos) {
+function hallIsClear(hallway, origin, homePos) {
     const step = (origin - homePos) / Math.abs(origin - homePos);
-    // cl('step: ', step)
-    for (let i = homePos; i < origin; i += step) {
-        // cl('testing: ', i);
+    for (let i = homePos; i != origin; i += step) {
         if (!isEmpty(hallway[i])) {
-            // cl('  ... breaking ...')
-            return false; // oh, now you're getting really ugly
+            return false;
         }
     }
     return true;
 }
 
 function* moveOut(origHall, origRooms, cost, bestCost, history) {
-    // cl('... left ampi out ...', history);
-    for (let i = 0; i < origRooms.length; i++) {
-        if (!roomDone(origRooms, i)
-            && !roomHalfDone(origRooms, i)
-            && !roomEmpty(origRooms, i)
-        ) {
-            const destinations = hwDestinations(origHall, origRooms[i].pos);
+    for (let rmIdx = 0; rmIdx < origRooms.length; rmIdx++) {
+        if (!roomDone(origRooms, rmIdx) && hasVisitor(origRooms, rmIdx)) {
+            const destinations =
+                hallwayDestinations(origHall, origRooms[rmIdx].pos);
             for (let destination of destinations) {
                 let [hallway, rooms] = copy(origHall, origRooms);
-                const room = rooms[i];
+                const room = rooms[rmIdx];
                 let stepsOut;
-                if (!isEmpty(room.top)) {
-                    hallway[destination] = room.top;
-                    room.top = empty;
-                    stepsOut = 1;
-                } else {
-                    // cl(room.bottom, isEmpty(origRooms[i].top), )
-                    hallway[destination] = room.bottom;
-                    room.bottom = empty;
-                    stepsOut = 2;
-                }
+
+                let i = 0;
+                for (; isEmpty(room[i]); i++);
+                hallway[destination] = room[i];
+                room[i] = empty;
+                stepsOut = i + 1;
+
                 const newCost = cost + ampiCost(hallway[destination]) *
                     (stepsOut + Math.abs(room.pos - destination));
-                yield* burrowCost(hallway, rooms, newCost, bestCost, history);
+                yield*
+                    burrowCost(hallway, rooms, newCost, bestCost, history);
             }
         }
     }
 }
 
-function* hwDestinations(hallway, start) {
+function* hallwayDestinations(hallway, start) {
     for (let i = start + 1; i < hallway.length; i++) {
         if (!isLobby(i)) {
             if (!isEmpty(hallway[i])) {
@@ -144,21 +140,21 @@ function allRoomsDone(rooms) {
 }
 
 function roomDone(rooms, ampiHome) {
-    return ampiHome === rooms[ampiHome].bottom
-        && ampiHome === rooms[ampiHome].top;
-}
-
-function roomHalfDone(rooms, ampiHome) {
-    return ampiHome === rooms[ampiHome].bottom
-        && isEmpty(rooms[ampiHome].top);
-}
-
-function roomEmpty(rooms, ampiHome) {
-    const r = isEmpty(rooms[ampiHome].bottom);
-    if (r && !isEmpty(rooms[ampiHome].top)) {
-        throw 'bad dev';
+    for (const val of rooms[ampiHome]) {
+        if (val != ampiHome) {
+            return false;
+        }
     }
-    return r
+    return true;
+}
+
+function hasVisitor(rooms, ampiHome) {
+    for (const val of rooms[ampiHome]) {
+        if (!isEmpty(val) && val != ampiHome) {
+            return true;
+        }
+    }
+    return false;
 }
 
 function isLobby(i) {
@@ -180,8 +176,11 @@ function isEmpty(x) {
 function copy(hallway, rooms) {
     return [
         hallway.slice(0),
-        rooms.map(room =>
-            ({ pos: room.pos, top: room.top, bottom: room.bottom }))
+        rooms.map(room => {
+            const newRoom = room.slice(0);
+            newRoom.pos = room.pos;
+            return newRoom;
+        })
     ];
 }
 
@@ -195,16 +194,22 @@ function ampiCost(ampi) {
 
 function displayHistory(history) {
     let prevCost = 0;
-    const c = (ascii) => String.fromCharCode(ascii + 65);
     for (const [hallway, rooms, cost] of history) {
         const diff = cost - prevCost;
         prevCost = cost;
         cl(' ');
         cl(`cost: ${cost} (+ ${diff})`);
-        cl('#############');
-        cl('#' + hallway.map(c).join('') + '#');
-        cl(`###${c(rooms[0].top)}#${c(rooms[1].top)}#${c(rooms[2].top)}#${c(rooms[3].top)}###`)
-        cl(`  #${c(rooms[0].bottom)}#${c(rooms[1].bottom)}#${c(rooms[2].bottom)}#${c(rooms[3].bottom)}#`)
-        cl('  #########');
+        displayBurrow(hallway, rooms);
     }
+}
+
+function displayBurrow(hallway, rooms) {
+    const toChar = (ascii) => String.fromCharCode(ascii + 65);
+    cl('#############');
+    cl('#' + hallway.map(toChar).join('') + '#');
+    cl(`###${toChar(rooms[0][0])}#${toChar(rooms[1][0])}#${toChar(rooms[2][0])}#${toChar(rooms[3][0])}###`)
+    for (let i = 1; i < rooms[0].length; i++) {
+        cl(`  #${toChar(rooms[0][i])}#${toChar(rooms[1][i])}#${toChar(rooms[2][i])}#${toChar(rooms[3][i])}#`);
+    }
+    cl('  #########');
 }
